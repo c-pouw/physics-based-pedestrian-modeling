3f6cff5fee72f6058d9c25b8c562d15ea859fa6c
c-pouw <@users.noreply.github.com>
2024-07-30 16:18:02 +0200

    Add napoleon extension to Sphinx for Google style docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     "matplotlib.sphinxext.plot_directive",
     "numpydoc",
     "sphinx_copybutton",
+    "sphinx.ext.napoleon",
 ]
 
 # Configuration options for plot_directive. See:
