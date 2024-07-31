bc768d3abb7993d3663dbdb61ec7ce38c7b5fb38
c-pouw <@users.noreply.github.com>
2024-07-31 09:12:53 +0200

    remove napoleon sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,6 @@ extensions = [
     "matplotlib.sphinxext.plot_directive",
     "numpydoc",
     "sphinx_copybutton",
-    "sphinx.ext.napoleon",
 ]
 
 # Configuration options for plot_directive. See:
